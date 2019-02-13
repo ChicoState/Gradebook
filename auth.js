8315dbf7d67772d9689ac245768617fd0db55730
Remington Breeze <remington.breeze@gmail.com>
2019-02-13 12:53:07 -0800

    Added some dependencies, and created functional authentication routes. Relies on a .env file for configuration (not included in repo)

---
+++ auth.js
@@ -0,0 +1,23 @@
+var jwt = require('jsonwebtoken')
+
+let secret = process.env.TOKEN_SECRET
+
+function authCheck(req, res, next) {
+  
+  var header_token = req.headers['x-access-token']
+  var cookie_token = req.cookies['csrf_token']
+  
+  if (!header_token || !cookie_token)
+    return res.status(403).send({ auth: false, message: 'No token provided.' })
+  else if (header_token != cookie_token)
+    return res.status(403).send({ auth: false, message: 'Tokens do not match.' })
+  jwt.verify(cookie_token, secret, function(err, decoded) {
+    if (err)
+      return res.status(403).send({ auth: false, message: 'Failed to authenticate token.' })
+    req.userId = decoded.id
+    next()
+  })
+
+}
+
+module.exports = authCheck
--- index.js
+++ index.js
@@ -1,6 +1,30 @@
+// import .env file
+require('dotenv').config()
+
+// initialize app and router
 var express = require('express')
 var app = express()
 var router = express.Router()
+
+// import dependencies
+const bcrypt = require('bcryptjs')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const jwt = require('jsonwebtoken')
+
+// import mongoose models
+let User = require('./models/user')
+
+// import auth middleware 
+let authCheck = require('./auth')
+
+// connect to mongoDB database
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true })
+.catch((err) => { 
+  console.log("Error connecting to database:")
+  console.log(err) 
+});
 
 let port = 3993
 
@@ -11,10 +35,79 @@ console.log("Server listening on port " + port)
 // serve static files from the client directory 
 app.use(express.static('client/build'))
 
+// get data from body parameters
+// parse application/json
+app.use(bodyParser.json({limit: '50mb'}));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// use cookieParser 
+app.use(cookieParser())
+
 //
 router.get('/', (req, res) => {
   res.send("Hello world!")
 })
 
+// testing auth
+router.get('/protected', authCheck, (req, res) => {
+  res.send(req.userId)
+})
+
+// login route
+router.post('/login', async function(req, res) {
+
+  let user = await User.findOne({ email: req.body.email })
+  if (!user) res.send("User not found")
+
+  else {
+    bcrypt.compare(req.body.password, user.password).then(function(validPassword) {
+      if (!validPassword) res.status(400).send({ auth: false, error: 'Incorrect password.', token: null })
+      else {
+        var token = jwt.sign({ 
+          id: user._id
+        }, process.env.TOKEN_SECRET, { expiresIn: 86400 })
+        res
+          .cookie('csrf_token', token, { maxAge: 86400000, httpOnly: true, domain: cookie_domain })
+          .status(200)
+          .send({ auth: true, token: token })
+      }
+    }).catch((err) => {
+      console.log("Error comparing passwords:")
+      console.log(err)
+      res.send({ auth: false, error: 'Something went wrong on the server.', token: null })
+    });
+  }
+
+});
+
+router.post('/user', async function(req, res) {
+  // make sure a user with that email doesn't already exist
+  let user = await User.findOne({ 'email' : req.body.email })
+  if (user) res.send("User already exists")
+
+  else {
+    // hash password
+    var hashedPassword = bcrypt.hashSync(req.body.password, 8)
+
+    let newUser = await User.create({
+      password: hashedPassword,
+      email: req.body.email,
+      name: req.body.name
+    })
+
+    // create auth token
+    var token = jwt.sign({ id: newUser._id }, process.env.TOKEN_SECRET, { expiresIn: 86400 });
+
+    res
+      .cookie('csrf_token', token, { maxAge: 86400000, httpOnly: true })
+      .status(200)
+      .send({ auth: true, token: token });
+  }
+}); 
+
 // serve all routes with the /api prefix
 app.use('/api', router)
+
+module.exports = app

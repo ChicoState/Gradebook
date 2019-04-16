423c28a113e53d2340cd7e25fc9501183fd28fdc
Remington Breeze <remington.breeze@gmail.com>
2019-04-16 10:20:42 -0700

    Homepage and navbar tweaks

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
+import React from 'react'
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import axios from 'axios'
+
 import Signup from './Signup.js'
 import Login from './Login.js'
 
 import StudentCourses from './student/Courses.js'
+
 import TeacherCourses from './teacher/Courses.js'
-
 import TeacherCourse from './teacher/Course.js'
-
 import TeacherGrades from './teacher/Grades.js'
 
 import './App.css';
 
-import { isLoggedIn, logout } from './auth'
+import { isLoggedIn, logout, getHeader } from './auth'
 
 class App extends React.Component {
 
   constructor(props) {
     super(props)
     this.state = {
-      loggedIn: isLoggedIn()
+      loggedIn: isLoggedIn(), 
+      user: {}
     }
+  }
+
+  async componentDidMount() {
+    let user = await axios.get('/user/me', { headers: getHeader() })
+    this.setState({ user: user })
   }
 
   render() { 
@@ -43,11 +50,15 @@ class App extends React.Component {
                   </li>
                 </div>
               }
-              { this.state.loggedIn && 
+              { this.state.loggedIn && !this.state.user.student  && 
                 <div> 
                   <li className="nav-item">
-                    <Link to="/account">Account</Link>
+                    <Link to="/teacher/courses">Courses</Link>
                   </li>
+                </div> 
+              }
+              { this.state.loggedIn && 
+                <div> 
                   <li className="nav-item">
                     <a href="#" onClick={logout}>Log Out</a>
                   </li>
@@ -82,7 +93,12 @@ class App extends React.Component {
 
 const Home = () => {
   return (
-    <h1> Welcome to Gradebook. </h1>
+    <div> 
+      <h1> Welcome to GradeBook. </h1>
+      <div className="mb-3"> Simple, easy to use grades for students and teachers. </div>
+      <Link to="/login" className="btn btn-light mr-3"> Log In </Link>
+      <Link to="/signup" className="btn btn-primary"> Sign Up </Link>
+    </div>
   )
 }
 

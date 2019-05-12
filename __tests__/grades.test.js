4479d53223a6de99f4096195cdd6a57aa4872f78
Mark B <mboyko@mail.csuchico.com>
2019-05-12 15:55:49 -0700

    Added classes.js to files

--- __tests__/grades.test.js
+++ __tests__/grades.test.js
@@ -10,7 +10,51 @@ describe("Get Invalid Course", () => {
 
 describe("Get Invalid Course", () => {
     test("Should Respond 404", async () => {
-        const res = await request(app).get("/api/course/not_valid_id");
+        const res = await request(app).get("/api/grade/not_valid_id");
         expect(res.statusCode).toBe(404);
     });
 });
+
+/*
+
+describe("Post Valid Assignment", () => {
+  const assignment = {
+    class_id: "EECE 555",
+    teacher_id: "test_teacher_id",
+    name: "Final Project",
+    pointsPossible: 100,
+    type: "Project"
+  };
+
+  test("Should Respond 200", async () => {
+  const res = await request(app).post("/api/assignment");
+  expect(res.statusCode).toBe(200);
+  expect(res.body).toBe({
+    class_id: "EECE 555",
+    teacher_id: "test_teacher_id",
+    name: "Final Project",
+    pointsPossible: 100,
+    type: "Project"
+  });
+  });
+});
+
+describe("Post Invalid Assignment", () => {
+  const assignment = {
+    class_id: "EECE 555",
+    name: "Final Project",
+  };
+  test("Should Respond 200", async () => {
+  const res = await request(app).post("/api/assignment");
+  expect(res.statusCode).toBe(404);
+  });
+});
+
+describe("Get Invalid Assignment", () => {
+  test("Should Respond 404", async () => {
+  const res = await request(app).get("/api/assignment/not_valid_id");
+  expect(res.statusCode).toBe(404);
+  });
+});
+
+*/

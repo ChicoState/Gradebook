b5e743e75e7472177bae9c1410c8640e3b5ebe09
Remington Breeze <remington.breeze@gmail.com>
2019-05-12 18:13:48 -0700

    implemented new style for student view

--- client/src/student/Courses.js
+++ client/src/student/Courses.js
@@ -13,7 +13,8 @@ class Courses extends Component {
       custom_id: "", 
       courses: [],
       user: {}, 
-      join_code: ""
+      join_code: "", 
+      colors: ['blue', 'red', 'green', 'yellow', 'orange']
     }
 
     this.handleInputChange = this.handleInputChange.bind(this)
@@ -49,22 +50,28 @@ class Courses extends Component {
   render () {
     return(
       <div> 
-        <h2 className="mt-2 mb-2"> Welcome, { this.state.user.name }! </h2> 
-        <h4 className="mt-3 mb-0"> Your Courses </h4> 
-        <div className="courses mb-4"> 
+        <h2 className="mt-2 mb-2"> Welcome, { this.state.user.name } </h2> 
+        <h4 className="mt-3 mb-1 small"> Your Courses </h4> 
+        <div className="courses row mb-4"> 
           { this.state.courses.map((c, i) => {
             return (
-              <div className="course d-flex align-items-center py-2" key={c._id}>
-                <div className="badge badge-primary mr-3">{c.grade * 100}%</div>
-                <div className="courseName mr-3"> <Link to={ '/user/class/' + c.custom_id }>{ c.name }</Link> </div> 
-                <div className="courseId"> { c.custom_id } </div>  
+              <div className="course col-md-6 col-12 d-flex align-items-center py-2" key={c._id}>
+                <div className={ "course-container d-flex " + this.state.colors[i % 5] }>
+                  <div className="">
+                    <div className="courseName">
+                      <Link to={ '/student/course/' + c.custom_id }>{ c.name }</Link>
+                    </div>
+                    <div className="courseId"> { c.custom_id } </div>  
+                  </div>
+                  <div className="ml-auto percentage">{Math.round(c.grade * 1000) / 10}%</div>                 
+                </div>
               </div>
             )
           })}
         </div>
         <div> 
-          <h4> Add Courses </h4> 
-          <div className="input-group">
+          <h4 className="small"> Add Course </h4> 
+          <div className="input-group w-50">
             <input className="form-control" type="text" name="join_code" value={this.state.join_code} placeholder="Join Code" onChange={this.handleInputChange}/>
             <div className="input-group-append">
               <div className="btn btn-primary" onClick={this.joinCourse}> Join </div> 
